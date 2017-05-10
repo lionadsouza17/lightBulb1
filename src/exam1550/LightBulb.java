361dd69745269619a3d33c58236468de53db936f
VeNoM lor <aatish93@gmail.com>
2017-05-10 10:53:17 +0530

    hey 1 working!

---
+++ src/exam1550/LightBulb.java
@@ -0,0 +1,27 @@
+package exam1550;
+
+public class LightBulb {
+	
+	int watts;
+	String type;
+	String color;
+	
+	
+	int getBulbWattage(int watts)
+	{
+		return watts;
+		
+	}
+	
+	String getBulbType(String type)
+	{
+		return type;
+		
+	}
+	String getBulbColor(String color)
+	{
+		return color;
+		
+	}
+
+}
